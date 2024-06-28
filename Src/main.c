0eb61d9b2517a446b02efc1ed97e111ff807c0b5
Kaushik Narayan <narayankaushik49@gmail.com>
2024-06-28 09:20:28 +0530

    Commit project

---
+++ Src/main.c
@@ -0,0 +1,47 @@
+/*
+ * Bare metal C code to toggle the user LED on STM32 Nucleo board using the on-board push button
+ */
+
+#include "stm32f4xx.h"
+
+#define GPIOC_EN		(1U<<2) //Write to register for port C clock enable
+#define GPIOA_EN        (1U<<0) //Write to register for port A clock enable
+
+#define PIN5		(1U<<5)
+#define PIN13		(1U<<13)
+
+#define LED_PIN			PIN5
+#define BTN_PIN			PIN13
+
+int main(void){
+
+	//Enable clock for GPIO A and C
+	RCC->AHB1ENR |= GPIOA_EN;
+	RCC->AHB1ENR |= GPIOC_EN;
+
+	//Set mode of pin13 on port C to input
+	GPIOC->MODER &= ~(1U<<26);
+	GPIOC->MODER &= ~(1U<<27);
+
+	//Set mode of pin5 on port A to output
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+
+	while(1){
+
+		//Check if button is pressed
+
+		if(GPIOC->IDR & BTN_PIN){
+
+			//Turn off LED
+			GPIOA->BSRR = LED_PIN;
+		}
+		else{
+
+			//Turn off
+			GPIOA->BSRR = (1U<<21);
+		}
+	}
+
+
+}
